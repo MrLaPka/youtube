f351018524c23ff9ae00f76f64534cdbbd09be90
Egor <42974925+MrLaPka@users.noreply.github.com>
2018-11-25 20:44:20 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -7,15 +7,22 @@ let request = new XMLHttpRequest();
 request.open('GET', URL, true);
 let massVids = [];
 let vidsId = [];
+let begin;
+let end;
+let ul = document.createElement('ul');
 request.onload = function() {
    let data = JSON.parse(request.responseText);
    massVids = [...data.items];
-   for(let i=0;i<massVids.length;i++){
-       vidsId[i] = massVids[i].id.videoId;
-   }
-   
-let idies = vidsId.join(',');
-let statistic = `https://www.googleapis.com/youtube/v3/videos?key=AIzaSyCZAKn50LUgqUl-F5tx414y8683angtook&id=${idies}&part=snippet,statistics`;
+let counterLi = 0;
+let pagination = document.getElementById('pagination');
+while(pagination.firstChild) {
+  pagination.removeChild(pagination.firstChild);
+};
+for(let i=0;i<massVids.length;i++){
+  vidsId[i] = massVids[i].id.videoId;
+}
+let idies0 = vidsId.slice(0,5).join(',');
+let statistic = `https://www.googleapis.com/youtube/v3/videos?key=AIzaSyCZAKn50LUgqUl-F5tx414y8683angtook&id=${idies0}&part=snippet,statistics`;
 let request2 = new XMLHttpRequest();
 request2.open('GET', statistic, true);
 let id1;
@@ -26,6 +33,34 @@ mainVid(id1);
 resultsLoop(data2);
 };
 request2.send();
+   for(let i=0;i<massVids.length;i++){
+    counterLi++;
+    vidsId[i] = massVids[i].id.videoId;
+    if(counterLi%5 === 0){
+     let li = document.createElement('li');
+     li.classList.add('lipages');
+     li.innerHTML = counterLi/5;
+   li.addEventListener('click', function(){
+  end = li.innerHTML*5;
+  begin = end-5;
+  idies = vidsId.slice(begin,end).join(',');
+  let statistic = `https://www.googleapis.com/youtube/v3/videos?key=AIzaSyCZAKn50LUgqUl-F5tx414y8683angtook&id=${idies}&part=snippet,statistics`;
+let request2 = new XMLHttpRequest();
+request2.open('GET', statistic, true);
+let id1;
+request2.onload = function() {
+let data2 = JSON.parse(request2.responseText);
+id1 = data2.items[0].id;
+mainVid(id1);
+resultsLoop(data2);
+};
+request2.send();
+   });
+     ul.appendChild(li);
+   }
+  }
+  
+pagination.appendChild(ul);
 };
 
 request.send();
@@ -33,6 +68,8 @@ request.send();
   function mainVid(id) {
     document.getElementById('video').innerHTML = `<iframe width='560' height='315' src='https://www.youtube.com/embed/${id}' frameborder='0' allow='autoplay; encrypted-media' allowfullscreen></iframe>`;
 }
+
+
 function resultsLoop(data) {
   let ls = document.getElementById('list');
   while(ls.firstChild) {
@@ -49,12 +86,12 @@ function resultsLoop(data) {
         let div = document.createElement('div');
    
         div.innerHTML =  `
-          <article data-key="${vid}">
+          <article class='newarticles' data-key="${vid}">
             <div class='details'>
-            <h4><a href = 'https://www.youtube.com/watch?v=${vid}' target='_blank'>${title}</a></h4>
-            <img src="${thumb}" alt='' class='thumb'>
-              <p><strong>Описание:</strong> ${desc}</p>
-              <p><strong>Канал:</strong> ${channelTit}</p>
+            <h4><a href = 'https://www.youtube.com/watch?v=${vid}' class = "listhrefs transition"target='_blank'>${title}</a></h4>
+            <img src="${thumb}" alt='Заставка видео' class='thumb transition'>
+              <p><strong>Описание:</strong> <span class = 'todescription'>${desc}</span></p>
+              <p class = 'tochannelid'><strong>Канал:</strong> ${channelTit}</p>
 <p><strong>Дата публикации:</strong> ${publication}</p>
 <p><strong>Количество просмотров:</strong> ${views}</p>
             </div>
@@ -68,6 +105,10 @@ document.addEventListener('click', function(e) {
   let el = e ? e.target : window.event.srcElement;
   while (el.getAttribute("data-key") === null) {
     el = el.parentNode || el.parentElement;
+  }
+  let top = Math.max(document.body.scrollTop,document.documentElement.scrollTop);
+  if(top > 0) {
+    window.scrollBy(0,-1500);
   }
   let id = el.getAttribute("data-key");
     mainVid(id);
